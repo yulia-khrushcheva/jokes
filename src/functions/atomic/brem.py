92cdbd103f0e23e860852eabf486160f5aae035c
Efrem199 <efrempo@yandex.ru>
2025-01-11 13:53:32 +0300

    Test2

--- src/functions/atomic/brem.py
+++ src/functions/atomic/brem.py
@@ -5,6 +5,7 @@ from telebot import types
 from telebot.callback_data import CallbackData
 from bot_func_abc import AtomicBotFunctionABC
 
+
 class AtomicExampleBotFunction(AtomicBotFunctionABC):
     """Модуль для получения цитат из сериала Breaking Bad через Telegram бота."""
 
@@ -22,7 +23,9 @@ class AtomicExampleBotFunction(AtomicBotFunctionABC):
 
     def set_handlers(self, bot: telebot.TeleBot):
         self.bot = bot
-        self.example_keyboard_factory = CallbackData('t_key_button', prefix=self.commands[0])
+        self.example_keyboard_factory = CallbackData(
+            't_key_button', prefix=self.commands[0]
+        )
 
         @bot.message_handler(commands=self.commands)
         def send_quote(message: types.Message):
@@ -32,19 +35,24 @@ class AtomicExampleBotFunction(AtomicBotFunctionABC):
                 bot.send_message(message.chat.id, "Пожалуйста, укажите количество цитат. Пример: /quote 3")
                 return
 
-            quotes = []
-            for _ in range(num_quotes):
-                response = requests.get(
-                    "https://api.breakingbadquotes.xyz/v1/quotes", timeout=10
-                )
-                if response.status_code == 200:
-                    data = response.json()[0]
-                    quote = data['quote']
-                    author = data['author']
-                    quotes.append(f"Цитата: {quote}\nАвтор: {author}")
-                else:
-                    bot.send_message(message.chat.id, "Не удалось получить цитату.")
-                    return
+            quotes = self.get_quotes(num_quotes)
+            if not quotes:
+                bot.send_message(message.chat.id, "Не удалось получить цитату.")
+                return
 
             for quote in quotes:
                 bot.send_message(message.chat.id, quote)
+
+    def get_quotes(self, num_quotes: int) -> List[str]:
+        """Получает цитаты из API Breaking Bad."""
+        quotes = []
+        for _ in range(num_quotes):
+            response = requests.get(
+                "https://api.breakingbadquotes.xyz/v1/quotes", timeout=10
+            )
+            if response.status_code == 200:
+                data = response.json()[0]
+                quote = data['quote']
+                author = data['author']
+                quotes.append(f"Цитата: {quote}\nАвтор: {author}")
+        return quotes
