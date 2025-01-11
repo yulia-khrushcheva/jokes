abe6ed27bc95f1b99413c467f742ad48ded32b9f
Diatlov Maxim <tegrasgt@gmail.com>
2025-01-11 13:48:01 +0300

    Update openlibrary.py

--- src/functions/atomic/openlibrary.py
+++ src/functions/atomic/openlibrary.py
@@ -5,40 +5,38 @@ import telebot
 from telebot import types
 from telebot.callback_data import CallbackData
 import requests
-from src.bot_func_abc import AtomicBotFunctionABC
+from bot_func_abc import AtomicBotFunctionABC
 
 class AtomicExampleBotFunction(AtomicBotFunctionABC):
     """Open Library API"""
 
-    commands: List[str] = ["find", "author"]
-    authors: List[str] = ["Diatlov Maksim"]
+    commands: List[str] = ["find_book", "find_author"]
+    authors: List[str] = ["@tegrasgt"]
     state: bool = True
-
-    bot: telebot.TeleBot
+    about: str = "Интеграция с Open Library"
+    description: str = """Реализация поиска по книге и автору в API Open Library"""
     example_keyboard_factory: CallbackData
+    bot: telebot.TeleBot
 
     def set_handlers(self, bot: telebot.TeleBot):
-        """Хэндлеры"""
+        """Модуль хэндлеров для обращения к Open Library"""
         self.bot = bot
         self.example_keyboard_factory = CallbackData('t_key_button', prefix=self.commands[0])
-        @bot.message_handler(commands=["find"])
-        def find_book_by_name(message: types.Message):
+
+        def __find_book_by_name(message):
             name = "+".join(message.text.replace(" ", "+").split("+")[1:])
-            print(name)
             req = ("https://openlibrary.org/search.json?q=" + name +
                    "&page=1&limit=1&mode=everything")
             r = requests.get(url=req, timeout=5)
             bookdata = r.json()
-            print(bookdata)
             reply = (f"Автор: {bookdata['docs'][0]['author_name'][0]}, \nГод издания: "
                      f"{bookdata['docs'][0]['first_publish_year']}, "
                      f"\nСреднее количество страниц: "
                      f"{bookdata['docs'][0]['number_of_pages_median']}\n")
-            bot.send_photo(caption=reply, photo="https://covers.openlibrary.org/b/OLID/" + str(
+            self.bot.send_photo(caption=reply, photo="https://covers.openlibrary.org/b/OLID/" + str(
                 dict(bookdata)["docs"][0]["cover_edition_key"]) + "-L.jpg", chat_id=message.chat.id)
 
-        @bot.message_handler(commands=["author"])
-        def find_book_by_author(message: types.Message):
+        def __find_book_by_author(message):
             name = "+".join(message.text.replace(" ", "+").split("+")[1:])
             print(name)
             req = ("https://openlibrary.org/search/authors.json?q=" +
@@ -58,5 +56,15 @@ class AtomicExampleBotFunction(AtomicBotFunctionABC):
             bot.send_photo(caption=reply, photo="https://covers.openlibrary.org/a/OLID/" + str(
                 dict(bookdata)["docs"][0]["key"]) + "-L.jpg", chat_id=message.chat.id)
 
-    def i_hate_linters(self):
-        """"Линтер сказал его добавить, потому что он ничего не понимает"""
+        @bot.message_handler(commands=[self.commands[0]])
+        def find_book_by_name(message: types.Message):
+            """Поиск по книге"""
+            __find_book_by_name(message)
+
+        @bot.message_handler(commands=[self.commands[1]])
+        def find_book_by_author(message: types.Message):
+            """Поиск по автору"""
+            __find_book_by_author(message)
+
+
+
