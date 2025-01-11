5365a5549bc1d6b6264f35b98d64f50ba74ce3e6
brokenk1d <228denver228@gmail.com>
2025-01-11 13:29:36 +0300

    ready

--- src/functions/atomic/dog_facts.py
+++ src/functions/atomic/dog_facts.py
@@ -1,47 +1,70 @@
-import os
+"""
+Модуль для реализации функции бота для получения интересных фактов о собаках.
+"""
+
 import logging
 from typing import List
 import telebot
 import requests
 from telebot import types
-from telebot.callback_data import CallbackData
 from bot_func_abc import AtomicBotFunctionABC
 
 class DogFactBotFunction(AtomicBotFunctionABC):
-    """Реализация функции бота для получения интересных фактов о собаках.
+    """
+    Реализация функции бота для получения интересных фактов о собаках.
     """
 
     commands: List[str] = ["dogfact", "df"]
     authors: List[str] = ["IHVH"]
-    about: str = "Интересные факты о собаках."  # Измененная строка
-    description: str = """Эта функция позволяет получать интересные факты о собаках. Просто вызовите команду /dogfact, и бот предоставит вам увлекательный факт. Узнайте больше о наших четвероногих друзьях!"""  # Измененная строка
+    about: str = "Интересные факты о собаках."
+    description: str = ("Эта функция позволяет получать интересные факты о "
+                        "собаках. Просто вызовите команду /dogfact, и бот "
+                        "предоставит вам увлекательный факт. Узнайте больше о "
+                        "наших четвероногих друзьях!")
     state: bool = True
 
-    bot: telebot.TeleBot
-    dog_fact_keyboard_factory: CallbackData
+    DOG_FACT_API_URL = "https://dog-api.kinduff.com/api/facts"
+
+    def __init__(self):
+        super().__init__()
+        self.bot = None
 
     def set_handlers(self, bot: telebot.TeleBot):
-        self.bot = bot
-        self.dog_fact_keyboard_factory = CallbackData('t_key_button', prefix=self.commands[0])
+        """
+        Устанавливает обработчики команд для бота.
 
-        @bot.message_handler(commands=self.commands)
+        :param bot: Экземпляр TeleBot.
+        """
+        self.bot = bot
+
+        @self.bot.message_handler(commands=self.commands)
         def dog_fact_message_handler(message: types.Message):
             chat_id_msg = f"\nCHAT ID = {message.chat.id}"
             try:
-                url = "https://dog-api.kinduff.com/api/facts"
-                response = requests.get(url)
+                response = requests.get(
+                    DogFactBotFunction.DOG_FACT_API_URL,
+                    timeout=5  # Добавлен таймаут
+                )
 
                 if response.status_code == 200:
                     facts_data = response.json()
                     fact_text = facts_data['facts'][0]
 
                     msg = (
-                        f"Ваш запрос обработан в DogFactBotFunction! {chat_id_msg}\n"
+                        f"Ваш запрос обработан в DogFactBotFunction!"
+                        f"{chat_id_msg}\n"
                         f"ФАКТ: {fact_text}"
                     )
-                    bot.send_message(chat_id=message.chat.id, text=msg)
+                    self.bot.send_message(chat_id=message.chat.id, text=msg)
                 else:
-                    bot.send_message(chat_id=message.chat.id, text="Произошла ошибка при получении факта.")
-            except Exception as e:
-                logging.error(f"Произошла ошибка при запросе к API: {e}")
-                bot.send_message(chat_id=message.chat.id, text="К сожалению, возникла ошибка при общении с сервером. Попробуйте позже.")
+                    self.bot.send_message(chat_id=message.chat.id, text="Произошла ошибка.")
+            except requests.exceptions.RequestException as e:
+                logging.error("Произошла ошибка при запросе к API: %s", e)
+                self.bot.send_message(chat_id=message.chat.id, text="Попробуйте позже.")
+    def check_bot_state(self) -> bool:
+        """
+        Проверяет состояние бота перед выполнением основной функции.
+
+        :return: True, если состояние бота активно, иначе False.
+        """
+        return self.state
