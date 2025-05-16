70cf53541c2aa84c818f867eab771f93563fe98e
SailentBlock <48534269+Kylon2308@users.noreply.github.com>
2025-05-16 23:17:36 +0300

    Update factSvN.py

--- src/functions/atomic/factSvN.py
+++ src/functions/atomic/factSvN.py
@@ -1,17 +1,17 @@
-from telebot import types
+from bot_func_abc import AtomicBotFunctionABC
 import requests
 
-class FactSvNFunction:
+class FactSvNFunction(AtomicBotFunctionABC):
     commands = ["factSvN"]
     authors = ["Kylon2308"]
     about = "Вывод случайного факта"
     description = "Команда /factSvN показывает случайный факт с внешнего API"
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self):
+        self.state = True  # Чтобы функция считалась активной
 
-    def register_handlers(self):
-        @self.bot.message_handler(commands=self.commands)
+    def set_handlers(self, bot):
+        @bot.message_handler(commands=self.commands)
         def handle_fact(message):
             API_URL = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
             try:
@@ -22,4 +22,4 @@ class FactSvNFunction:
             except Exception as e:
                 text = f"Ошибка при получении факта: {e}"
 
-            self.bot.send_message(chat_id=message.chat.id, text=f"💡 Did you know?\n\n{text}")
+            bot.send_message(chat_id=message.chat.id, text=f"💡 Did you know?\n\n{text}")
